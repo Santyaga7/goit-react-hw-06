c136d6ec0988e7e865c7ccbb006ec2cc06b1be6c
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-06-16 18:05:17 +0300

    16.06

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
-import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {store,persistor}  from './redux/store.js'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </BrowserRouter>
+    </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
