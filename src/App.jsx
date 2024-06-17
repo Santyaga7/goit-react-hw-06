83da2b22ab0fd7798878f58e274636713e871865
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-06-17 11:31:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,15 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import { addContact, deleteContact, selectContacts } from "./redux/contactsSlice";
 import { changeFilter, selectNameFilter } from "./redux/filtersSlice";
 
-const App = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  const dispatch = useDispatch();
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  const handleAddContact = (newContact) => {
-    dispatch(addContact(newContact));
-  };
-
-  const handleDeleteContact = (contactId) => {
-    dispatch(deleteContact(contactId));
-  };
-
-  const handleFilterChange = (filter) => {
-    dispatch(changeFilter(filter));
-  };
-
+function App() {
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm onAddContact={handleAddContact} />
-      <SearchBox value={filter} onFilter={handleFilterChange} />
-      <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
+      <ContactsForm />
+      <SearchBox />
+      <ContactList />
     </div>
-  );
-};
+  )
+}
+export default App
 
-export default App;
