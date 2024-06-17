d422efa180fd77b77ff21d2f760cc782cb35f6db
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-06-17 11:59:34 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import { deleteContact, selectContacts } from '../../redux/contactsSlice';
 import css from './ContactList.module.css'
+import React, { useState } from 'react';
 
 const ContactList = () => {
   const dispatch = useDispatch();
@@ -11,20 +12,32 @@ const ContactList = () => {
   const handleDelete = (contactId) => {
     dispatch(deleteContact(contactId)); 
   };
+  const [filter, setFilter] = useState('');
+  const filteredContacts = contacts.filter(contact =>
+  contact.name.toLowerCase().includes(filter.toLowerCase())
+);
 
-  return (
+return (
+  <div>
+    <input
+      type="text"
+      placeholder="Фильтр по имени"
+      value={filter}
+      onChange={(e) => setFilter(e.target.value)}
+    />
     <ul className={css.contactContainer}>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <Contact
           key={contact.id}
           name={contact.name}
           number={contact.number}
           id={contact.id}
-          onDelete={() => handleDelete(contact.id)} 
+          onDelete={() => handleDelete(contact.id)}
         />
       ))}
     </ul>
-  );
-};
+  </div>
+);
 
 export default ContactList;
+
