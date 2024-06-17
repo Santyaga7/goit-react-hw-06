12321ff88fdd804f145eab882f418e1b16c28472
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-06-17 12:02:53 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,29 +1,40 @@
-
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import { deleteContact, selectContacts } from '../../redux/contactsSlice';
-import css from './ContactList.module.css'
+import css from './ContactList.module.css';
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
+  const [filter, setFilter] = useState(''); // Локальное состояние для фильтра
 
   const handleDelete = (contactId) => {
     dispatch(deleteContact(contactId)); 
   };
 
   return (
-    <ul className={css.contactContainer}>
-      {contacts.map(contact => (
-        <Contact
-          key={contact.id}
-          name={contact.name}
-          number={contact.number}
-          id={contact.id}
-          onDelete={() => handleDelete(contact.id)} 
-        />
-      ))}
-    </ul>
+    <div>
+      <input
+        type="text"
+        placeholder="Фильтр по имени"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <ul className={css.contactContainer}>
+        {contacts
+          .filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+          .map(contact => (
+            <Contact
+              key={contact.id}
+              name={contact.name}
+              number={contact.number}
+              id={contact.id}
+              onDelete={() => handleDelete(contact.id)} 
+            />
+          ))}
+      </ul>
+    </div>
   );
 };
 
